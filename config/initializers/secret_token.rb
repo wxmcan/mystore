091704e4e684eec1aad60345edb77ed875c459cf
bob <mysharebook.cn@gmail.com>
2011-12-25 18:14:52 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = 'e8d0cf1d0c5116c40ab4d28177a0897bf5cb8709df2066b9895484868daf4c71430e782c1acd2bad55098b2f398e70944ab071c9097be0b1af08c9869861915c'
