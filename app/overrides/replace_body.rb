3abfcc24503d3125a1c1798575f224e5c214b491
wxmcan <wxmcan@163.com>
2012-01-05 00:42:43 +0800

    add GA code

--- app/overrides/replace_body.rb
+++ app/overrides/replace_body.rb
@@ -91,4 +91,17 @@ Deface::Override.new(:virtual_path => %q{layouts/spree_application},
 
       </div>
     </footer>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28041586-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>})
